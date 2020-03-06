1a171e2a058832c0484346ea794300e173e6942c
Toshio Watanabe <toshio_ws@hotmail.com>
2020-03-06 19:10:40 -0300

    Projeto criado

---
+++ src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,62 @@
+package br.com.caelum.agenda.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.caelum.agenda.dao.ContatoDao;
+import br.com.caelum.agenda.modelo.Contato;
+
+@WebServlet("/adicionaContato")
+public class AdicionaContatoServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//busca o writter
+		PrintWriter out = resp.getWriter();
+		
+		//parametros do request
+		String nome = req.getParameter("nome");
+		String endereco = req.getParameter("endereco");
+		String email = req.getParameter("email");
+		String dataEmTexto = req.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+		
+		//conversao data
+		try {
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			
+			dataNascimento.setTime(date);
+			
+		} catch(ParseException e) {
+			out.println("Erro na conversão da data");
+			return; //para a execução do metodo
+		}
+		
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEndereco(endereco);
+		contato.setEmail(email);
+		contato.setDataNascimento(dataNascimento);
+		
+		//salva o contato
+		ContatoDao dao = new ContatoDao();
+		dao.adiciona(contato);
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato " + contato.getNome() + " adicionado com sucesso.");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
---
+++ src/br/com/caelum/servlet/OiMundo.java
@@ -0,0 +1,36 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OiMundo extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title> Primeira Servlet </title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Oi mundo Servlet!</h1>");
+		out.println("<h2>" + "Horas: " + sdf.format(Calendar.getInstance().getTime()) + "<h2>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		
+	}
+
+}
